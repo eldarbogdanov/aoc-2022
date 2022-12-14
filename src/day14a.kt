02263e4cb5baaa069950141d747ca41a9f6485ea
Eldar Bogdanov <eldar@asana.com>
2022-12-14 13:17:12 +0800

    Day 14

---
+++ src/day14a.kt
@@ -0,0 +1,52 @@
+fun main() {
+    val test = """498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9"""
+
+    val st: MutableSet<Pair<Int, Int>> = mutableSetOf();
+    for(s in test.split("\n")) {
+        val t = s.split(" -> ");
+        val points = t.map{ Pair(
+            Integer.parseInt(it.split(",")[0]),
+            Integer.parseInt(it.split(",")[1])
+        ) };
+        var last: Pair<Int, Int>? = null;
+        for(point in points) {
+            if (last == null) {
+                last = point;
+                continue;
+            }
+            if (last.first == point.first) {
+                for(i in Math.min(last.second, point.second)..Math.max(last.second, point.second)) {
+                    st.add(Pair(last.first, i));
+                }
+            } else {
+                for(i in Math.min(last.first, point.first)..Math.max(last.first, point.first)) {
+                    st.add(Pair(i, last.second));
+                }
+            }
+            last = point;
+        }
+    }
+    var ans = 0;
+    while(true) {
+        var x = 500;
+        var y = 0;
+        while(y < 200) {
+            if (!st.contains(Pair(x, y + 1))) {
+                ++y;
+            } else if (!st.contains(Pair(x - 1, y + 1))) {
+                --x;
+                ++y;
+            } else if (!st.contains(Pair(x + 1, y + 1))) {
+                ++x;
+                ++y;
+            } else break;
+        }
+        if (y == 200) {
+            break;
+        }
+        st.add(Pair(x, y));
+        ++ans;
+    }
+    println(ans);
+}
